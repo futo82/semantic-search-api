e794f0b97d54ed4960c38b96edbc9885f409bf86
Tony Fu <tony-fu@live.com>
2023-11-29 09:56:17 -0500

    Add semantic search api code, postman collection, and a few boolq examples

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# semantic-search-api
+# Semantic Search API
+
+This repository is based off of the book "Quick Start Guide to Large Language Models" by Sinan Ozdemir and contains a simple example of semantic search using OpenAI and Pinecone. There are two (2) RESTful endpoints, one to add document into a vector database and the other to query documents from the database.
+
+## Start API
+
+```
+pip install -r requirements.txt
+python api.py
+```
+
+## API
+
+#### POST /document/add
+
+This endpoint adds a document to the database.
+
+```
+curl -X POST \
+  http://127.0.0.1:8000/document/add \
+  -H 'Content-Type: application/json' \
+  -d '{
+	"text": "If one assumes that the unit variable cost is constant, as in cost-volume-profit analysis developed and used in cost accounting by the accountants, then total cost is linear in volume, and given by: total cost = fixed costs + unit variable cost * amount."
+}'
+```
+
+#### GET /documents/retrieve
+
+This endpoint query the database for relevant documents that matches the query.
+
+```
+curl -X POST \
+  http://127.0.0.1:8000/documents/retrieve \
+  -H 'Content-Type: application/json' \
+  -d '{
+	"query": "is all of thailand in the same time zone"
+}'
+```
+
+## Dataset
+
+The [BoolQ dataset](https://github.com/google-research-datasets/boolean-questions) was used to build out the the semantic search.
---
+++ api.py
@@ -0,0 +1,117 @@
+import hashlib
+import os
+import pinecone
+import uvicorn
+
+from datetime import datetime
+from dotenv import load_dotenv
+from fastapi import FastAPI, Response, status
+from openai import OpenAI
+from pydantic import BaseModel
+
+load_dotenv()
+
+client = OpenAI()
+
+# Create an openai client
+app = FastAPI()
+
+# Retrieve pinecone api key from environment variable
+pinecone_key = os.getenv("PINECONE_API_KEY")
+INDEX_NAME = 'semantic-search'
+
+# Create the pinecone index if it does not exist and get a reference to the index.
+if pinecone_key:
+    pinecone.init(api_key=pinecone_key, environment="gcp-starter")
+
+    if not INDEX_NAME in pinecone.list_indexes():
+        pinecone.create_index(
+            # The name of the index to create.
+            INDEX_NAME,
+            # The dimensionality of the vectors that will be indexed.
+            dimension=1536,
+            # The similarity metric to use when searching the index.
+            metric='cosine',
+            # The type of Pinecone pod to use for hosting the index.
+            pod_type="starter" 
+        )
+        print('Pinecone index created')
+    else:
+        print('Pinecone index already exists')
+
+    # Store the index as a variable
+    index = pinecone.Index(INDEX_NAME)
+
+# Call openai to create the vector embedding for the text
+def get_embedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+# Return the MD5 hash of the input string as a hexadecimal string
+def hash_str(s):
+    return hashlib.md5(s.encode()).hexdigest()
+
+class InsertRequest(BaseModel):
+    text: str
+
+class SearchRequest(BaseModel):
+    query: str
+    num_results: int = 3
+
+class DocumentResponse(BaseModel):
+    text: str
+    date_uploaded: datetime
+    score: float
+    id: str
+
+class SearchResponse(BaseModel):
+    documents: list
+
+@app.post("/document/add", status_code=status.HTTP_201_CREATED)
+async def add_document(request: InsertRequest, response: Response):
+    if not pinecone_key:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+    
+    embedding = get_embedding(request.text)
+    pinecone_request = [
+        (
+            # Generate a unique id for the passage
+            hash_str(request.text),
+            # The vector embedding of the string
+            embedding,
+            # A dictionary of metadata
+            dict(text=request.text, date_uploaded=datetime.utcnow())
+        )
+    ]
+
+    if (index.upsert(pinecone_request).get('upserted_count') != 1):
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+@app.post("/documents/retrieve", response_model=SearchResponse)
+async def retrieve_documents(request: SearchRequest):
+    if not pinecone_key:
+        return SearchResponse(documents=[])
+
+    query = request.query
+    query_embedding = get_embedding(query)
+
+    top_results = index.query(
+        vector=query_embedding,
+        top_k=request.num_results,
+        include_metadata=True
+    ).get('matches')
+
+    results = [
+        DocumentResponse
+        (
+            text=r['metadata']['text'],
+            date_uploaded=r['metadata']['date_uploaded'],
+            score=r['score'],
+            id=r['id']
+        ) for r in top_results
+    ]
+    return SearchResponse(documents=results)
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
